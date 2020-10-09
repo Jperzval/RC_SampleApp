2adb6493c8ee5d3d659e13406a82544d7e05d19e
Jperzval <perezvalle.jes@gmail.com>
2020-10-09 17:59:02 -0400

    created wrapper class for planets class

---
+++ app/src/main/java/com/example/rc_sampleapp/model/PlanetWrapper.kt
@@ -0,0 +1,3 @@
+package com.example.rc_sampleapp.model
+
+class PlanetWrapper (var solarSystem: List<Planets>)
--- app/src/main/java/com/example/rc_sampleapp/network/PlanetApi.kt
+++ app/src/main/java/com/example/rc_sampleapp/network/PlanetApi.kt
@@ -1,12 +1,13 @@
 package com.example.rc_sampleapp.network
 
+import com.example.rc_sampleapp.model.PlanetWrapper
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface PlanetApi {
 
     @GET(ENDPOINT)
-    fun getPlanetData(): Call<List<PlanetApi>>
+    fun getPlanetData(): Call<PlanetWrapper>
 
     companion object {
         const val ENDPOINT = "/Jperzval/RC_SampleApp/master/JSON?token=AKN5FF6Z5LYJK54MWBC75NC7QDNL2"
