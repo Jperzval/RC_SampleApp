e7b2b62c8879cbd36cfb0a48e3fb864bbb549c2f
Jperzval <perezvalle.jes@gmail.com>
2020-10-09 17:41:45 -0400

    created presenter class

---
+++ app/src/main/java/com/example/rc_sampleapp/presenter/PlanetPresenter.kt
@@ -0,0 +1,10 @@
+package com.example.rc_sampleapp.presenter
+
+import com.example.rc_sampleapp.MainContract
+
+class PlanetPresenter: MainContract.Presenter{
+
+    override fun getCall() {
+        TODO("Not yet implemented")
+    }
+}
